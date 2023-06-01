9561b49e83346c97a073dc5b39219d38da40bd90
Gina Brag <55889669+Coffeeva18@users.noreply.github.com>
2023-06-01 15:45:42 +0200

    styling login

--- frontend/login.php
+++ frontend/login.php
@@ -20,12 +20,9 @@ $page_title = "BlaBlaJU";
     </header>
 
 <form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
 
   <div class="container">
-    <label for="uname"><b>Username</b></label>
+    <label for="uname"><b>Email</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
 
     <label for="psw"><b>Password</b></label>
